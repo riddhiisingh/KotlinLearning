2e9320bca0600ba7a87ced7c9c0e80434be4e2de
riddhiisingh <riddhis2101@gmail.com>
2021-06-17 19:38:30 +0530

    Kotlin Learning

---
+++ src/ForLoop.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    println("Start app")
+
+    println("Start Ascending")
+    for (i in 1..20 step 2){
+        println("Number is $i")
+
+
+    }
+
+println("End Ascending")
+
+    println("Start Descending")
+for (i in  20 downTo 0 step 2) {//for descending
+    println("Number is $i")
+}
+
+    println("End Descending")
+}
---
+++ src/SimpleIf.kt
@@ -0,0 +1,12 @@
+fun main(){
+    print("Enter your age: ")
+    val age = readLine()!!.toInt()
+
+    if (age>60){
+
+        println("Your age is $age")
+    }
+    else
+
+    println("Your age is less than 60")
+}
